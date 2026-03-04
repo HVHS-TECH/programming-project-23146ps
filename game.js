af37980494e40fd87ead689e29a3ee9c97526586
23146ps <23146ps@hvhs.school.nz>
2026-03-04 15:14:56 +1300

    4/03/26

--- game.js
+++ game.js
@@ -11,26 +11,30 @@ function setup() {
 	cnv = new Canvas(1100, 900);
 
 	//player
-	sprite = new Sprite(width/2,height/2,50);
-	sprite.color = 'blue'
+	playerSprite = new Sprite(width/2,height/2,50);
+	playerSprite.color = 'blue'
 
-	let score = 0
+	roomScore = 0
 
 	//let roomType = 0
+	function scoreUp() {
+			roomScore +=1
+			console.log("room cleared!!")
+	} 
 
 	//door
-	exitDoor = new Sprite(width / 2, 0, 100, 20, 'k');
+	exitDoor = new Sprite(width / 2, 0, 100, 20, 's');
 	exitDoor.color = 'orange'
 
 	//walls
-	wallLH = new Sprite(0, height / 2, 8, height, 'k');
-	wallLH.color = 'black';
-	wallRH = new Sprite(width, height / 2, 8, height, 'k');
-	wallRH.color = 'black'
+	wallLeft = new Sprite(0, height / 2, 8, height, 'k');
+	wallLeft.color = 'black';
+	wallRight = new Sprite(width, height / 2, 8, height, 'k');
+	wallRight.color = 'black'
 	wallTop = new Sprite(width / 2, 0, width, 8, 'k');
 	wallTop.color = 'black'
-	wallBot = new Sprite(width / 2, height, width, 8, 'k');
-	wallBot.color = 'black'
+	wallBottom = new Sprite(width / 2, height, width, 8, 'k');
+	wallBottom.color = 'black'
 }
 	
 /*******************************************************/
@@ -62,37 +66,24 @@ function draw() {
 
 	// movement
 	if (kb.pressing('left')) {
-		sprite.vel.x = -4;
-	} 
-	
-	if (kb.pressing('right')){
-		sprite.vel.x = 4;
+		playerSprite.vel.x = -4;
+	} else if (kb.pressing('right')){
+		playerSprite.vel.x = 4;
+	} else {
+		playerSprite.vel.x = 0;
 	}
 
 	if (kb.pressing('up')){
-		sprite.vel.y = -4;
-	}
-	
-	if (kb.pressing('down')){
-        sprite.vel.y = 4;
-    }
-
-	if (kb.released('left')) {
-		sprite.vel.x = 0
-	}
-	
-	if (kb.released('right')){
-		sprite.vel.x = 0
+		playerSprite.vel.y = -4;
+	} else if (kb.pressing('down')){
+        playerSprite.vel.y = 4;
+    } else {
+		playerSprite.vel.y = 0;
 	}
 
-	if (kb.released('up')) {
-		sprite.vel.y = 0
-	}
-	
-	if (kb.released('down')){
-		sprite.vel.y = 0
-	}
+	playerSprite.collided(exitDoor, scoreUp())
 
+	//text("Rooms Cleared: " +score, 50, 50);
 }
 
 /*******************************************************/
