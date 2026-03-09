16434fb694d2e6957ff879d1ef5f044a27c0e770
23146ps <23146ps@hvhs.school.nz>
2026-03-10 12:30:15 +1300

    10/03/26 B

--- game.js
+++ game.js
@@ -17,8 +17,23 @@ function setup() {
 	gunSprite = new Sprite(playerSprite.x, playerSprite.y, 20, 20, 'n');
 	gunSprite.color ='green'
 
-	bulletSprite = new Sprite(playerSprite.x, playerSprite.y, 10, 'n')
-	bulletSprite.color = 'red'
+	//bulletSprite = new Sprite(playerSprite.x, playerSprite.y, 10, 'n')
+	//bulletSprite.color = 'red'
+
+	bulletGroup = new Group();
+
+	/*
+	for (i = 0; i < 40; i++) {
+		
+		bulletSprite = new Sprite(playerSprite.x, playerSprite.y, 10);
+		
+		bulletSprite.vel.x = randNum = random(2, 8);
+		
+		bulletSprite.vel.y = randNum = random(2, 8);
+		
+		bulletGroup.add(bulletSprite);
+	}
+	*/
 
 	roomScore = 0
 
@@ -29,14 +44,29 @@ function setup() {
 	exitDoor.color = 'orange'
 
 	//walls
+
+	wallGroup = new Group();
+
 	wallLeft = new Sprite(0, height / 2, 8, height, 'k');
 	wallLeft.color = 'black';
+
+	wallGroup.add(wallLeft);
+
 	wallRight = new Sprite(width, height / 2, 8, height, 'k');
 	wallRight.color = 'black'
+
+	wallGroup.add(wallRight);
+
 	wallTop = new Sprite(width / 2, 0, width, 8, 'k');
 	wallTop.color = 'black'
+
+	wallGroup.add(wallTop);
+
 	wallBottom = new Sprite(width / 2, height, width, 8, 'k');
 	wallBottom.color = 'black'
+
+	wallGroup.add(wallBottom);
+
 }
 
 function scoreUp() {
@@ -95,17 +125,29 @@ function draw() {
 	gunSprite.moveTowards(playerSprite.x, playerSprite.y, 1);
 	gunSprite.rotateTowards(mouseX, mouseY, 1);
 	if (kb.pressed('space')) {
-		bulletSprite.rotateTowards(mouseX, mouseY, 1);
-		bulletSprite.moveTo(mouseX, mouseY, 20);
+		
+		bulletSprite = new Sprite(playerSprite.x, playerSprite.y, 10);
+		
+		bulletSprite.moveTo(mouseX, mouseY, 50);
+
+		bulletGroup.add(bulletSprite);
+
 	} else {
 		//bulletSprite.moveTowards(playerSprite.x, playerSprite.y, 1);
+
+	bulletGroup.collides(wallGroup, func2Call)
+
+		function func2Call(bulletSprite, wallGroup) {
+		console.log("bullet colided")
+		// Delete the alien which was hit
+		bulletSprite.remove();
 	}
 
 	// door
 	if (playerSprite.collided(exitDoor)) {
 		scoreUp()
 		playerSprite.moveTo(width/2, 800, 10000);
-		setTimeout(100)
+		setTimeout(1000)
 	}
 }
 
