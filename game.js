275144bd4c8723d379b47419606000a32b8ecd74
23146ps <23146ps@hvhs.school.nz>
2026-03-24 12:29:37 +1300

    24/03/2026

--- game.js
+++ game.js
@@ -2,7 +2,7 @@
 // P5.play: project
 // Written by 23146ps
 /*******************************************************/
-	
+
 /*******************************************************/
 // setup()
 /*******************************************************/
@@ -11,17 +11,17 @@ function setup() {
 	cnv = new Canvas(1100, 900);
 
 	//player
-	playerSprite = new Sprite(width/2, height - 100, 40, 'd');
+	playerSprite = new Sprite(width / 2, height - 100, 40, 'd');
 	playerSprite.color = 'blue'
 
 	targetGroup = new Group();
-	for (let i = 0; i < 80; i++) { 
+	for (let i = 0; i < 80; i++) {
 		targetSprite = new Sprite(random(500), random(200), 20, 20);
 		targetGroup.add(targetSprite)
 	}
 
 	gunSprite = new Sprite(playerSprite.x, playerSprite.y, 20, 20, 'n');
-	gunSprite.color ='green'
+	gunSprite.color = 'green'
 
 	//bulletSprite = new Sprite(playerSprite.x, playerSprite.y, 10, 'n')
 	//bulletSprite.color = 'red'
@@ -50,100 +50,69 @@ function setup() {
 	wallGroup.add(wallLeft);
 
 	wallRight = new Sprite(width, height / 2, 8, height, 's');
-	wallRight.color = 'black'
+	wallRight.color = 'black';
 
 	wallGroup.add(wallRight);
 
 	wallTop = new Sprite(width / 2, 0, width, 8, 's');
-	wallTop.color = 'black'
+	wallTop.color = 'black';
 
 	wallGroup.add(wallTop);
 
 	wallBottom = new Sprite(width / 2, height, width, 8, 's');
-	wallBottom.color = 'black'
+	wallBottom.color = 'black';
 
 	wallGroup.add(wallBottom);
 
 }
 
 function scoreUp() {
-		roomScore +=1
-		console.log("room cleared!!")
-} 
+	roomScore += 1
+	console.log("room cleared!!");
+}
 
 bulletAngle = 0
 
 
-	
+
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
 	background('ccc');
 
-	text("rooms cleared: " +roomScore, 50, 50)
+	text("rooms cleared: " + roomScore, 50, 50)
+	text("targets left: " + targetGroup.length, 50, 75)
 
-	rayCastDirection = playerSprite.angleTo(mouseX, mouseY)
-
-	/*
-	targets.color = color(120);
-
-	let sprites = world.rayCast(playerSprite, rayCastDirection, 5000);
-
-	for (let s of sprites) s.color = 'orange';
-
-	stroke('orange');
-	line(playerSprite.x, playerSprite.y, mouse.x, mouse.y);
-	noStroke();
-	*/
-
-	/*
-	if (roomType = 1){
-		background('ccc');
-		roomOne()
-		roomType = random(2,3)
-	} 
-
-	function roomOne() {
-		wallLH = new Sprite(0, height / 2, 8, height, 'k');
-		wallLH.color = 'black';
-		wallRH = new Sprite(width, height / 2, 8, height, 'k');
-		wallRH.color = 'black'
-		wallTop = new Sprite(width / 2, 0, width, 8, 'k');
-		wallTop.color = 'black'
-		wallBot = new Sprite(width / 2, height, width, 8, 'k');
-		wallBot.color = 'black'
-	}
-	*/
-
-	//sprite.moveTowards(mouseX, mouseY, 10);
 
 	/*******************************************************/
 	// player movement
 	/*******************************************************/
 	if (kb.pressing('a')) {
 		playerSprite.vel.x = -4;
-	} else if (kb.pressing('d')){
+	} else if (kb.pressing('d')) {
 		playerSprite.vel.x = 4;
 	} else {
 		playerSprite.vel.x = 0;
 	}
 
-	if (kb.pressing('w')){
+	if (kb.pressing('w')) {
 		playerSprite.vel.y = -4;
-	} else if (kb.pressing('s')){
-        playerSprite.vel.y = 4;
-    } else {
+	} else if (kb.pressing('s')) {
+		playerSprite.vel.y = 4;
+	} else {
 		playerSprite.vel.y = 0;
 	}
 
-	// gun
 
+	/*******************************************************/
+	// shooting killing
+	/*******************************************************/
 	gunSprite.moveTowards(playerSprite.x, playerSprite.y, 1);
 	//gunSprite.moveTowards(mouseX, mouseY, 60);
 
 	if (kb.pressed('space')) {
-		
+
 		bulletSprite = new Sprite(gunSprite.x, gunSprite.y, 10, 'n');
 
 		bulletSprite.direction = playerSprite.angleTo(mouseX, mouseY)
@@ -153,29 +122,31 @@ function draw() {
 		bulletGroup.add(bulletSprite);
 
 	}
-		//bulletSprite.moveTowards(playerSprite.x, playerSprite.y, 1);
-	if (bulletGroup.overlaps(wallGroup, func2Call)) {}
-	if (bulletGroup.overlaps(targetGroup, murder)) {}
+	//bulletSprite.moveTowards(playerSprite.x, playerSprite.y, 1);
+	if (bulletGroup.overlaps(wallGroup, func2Call)) { }
+	if (bulletGroup.overlaps(targetGroup, murder)) { }
 
-		function func2Call(bulletSprite, wallGroup) {
+	function func2Call(bulletSprite, wallGroup) {
 		console.log("bullet colided")
 		bulletSprite.remove();
 	}
 
-	function murder(bulletSprite, targetSprite){
+	function murder(bulletSprite, targetSprite) {
 		console.log("kill")
 		bulletSprite.remove()
 		targetSprite.remove()
 	}
 
 
-	// door
-	if (playerSprite.overlaps(exitDoor)) {
+	/*******************************************************/
+	// levelchange
+	/*******************************************************/
+	if (playerSprite.overlaps(exitDoor) && targetGroup.length < 1) {
 		scoreUp()
 		playerSprite.moveTo(550, 850, 10000);
 
-		targetGroup.visible = false
-		targetGroup.physicsType = none
+		//targetGroup.visible = false
+		//targetGroup.physicsType = none
 
 		if (roomType = 1) {
 			background('ccc');
@@ -189,6 +160,9 @@ function draw() {
 		}
 
 		setTimeout(1000)
+	} else if (playerSprite.overlapping(exitDoor) && targetGroup.length > 0) {
+
+		text("oops! you need to destroy all the targets before progressing!", width / 2, height / 2);
 	}
 }
 
